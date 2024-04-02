cf4efbcf7d90fa3c3d5af0fb0b491a2b03703940
Justin <nguyen.thu.duc.trung@gmail.com>
2024-04-03 01:52:48 +0700

    [FEATURE] Add seeders

---
+++ src/database/seeders/20240402184532-seed_users.js
@@ -0,0 +1,24 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const salt = bcrypt.genSaltSync();
+    const password = bcrypt.hashSync('Aa@123456', salt);
+    const users = [];
+    for (let i = 0; i < 100; i++) {
+      users.push({
+        id: undefined,
+        fullName: `User #${i}`,
+        email: `nguyen.thu.duc.trung+${i}@gmail.com`,
+        password,
+        verificationAt: (Math.floor(Math.random() * 10) + 1) % 2 === 0 ? new Date() : undefined,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+    }
+    await queryInterface.bulkInsert('users', users, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+  },
+};
